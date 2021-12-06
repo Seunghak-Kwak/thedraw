8633b54dd12ee60ad88619550037ad1747c8dac0
Seunghak-Kwak <kdhak2@gmail.com>
2021-12-06 13:05:41 +0900

    freinds_json update

---
+++ test.js
@@ -0,0 +1,5 @@
+// const date = new Date().toLocaleString("ko-KR", {timeZone: "Asia/Seoul"}).split('.')
+// var p = new Date().toString().replace(/[A-Z]{3}\+/,'+').split(/ /);
+
+console.log(new Date().toLocaleString("en-US", {timeZone: "Asia/Seoul"}))
+console.log(new Date().toString())
