bd967692567374eb7df7b7f7568824751dd37028
Seunghak-Kwak <kdhak2@gmail.com>
2021-12-14 11:40:12 +0900

    send only draw_data

--- routes/send.js
+++ routes/send.js
@@ -44,31 +44,51 @@ router.post("/", async function (req, res, next) {
           });
           console.log(friends.data.elements);
 
-          // 메세지 보내기
+          // 메세지 보내기 (응모만)
           try {
             let sendme;
             let sendFriends;
             //send
-            if (today_data.length > 0) {
-              if (today_data.length > 1) {
-                sendme = api.sendme(token, today_data, "list");
-                sendFriends = api.sendFriends(token, today_data, idList, "list");
+            if (draw_data.length > 0) {
+              if (draw_data.length > 1) {
+                sendme = api.sendme(token, draw_data, "list");
+                sendFriends = api.sendFriends(token, draw_data, idList, "list");
               }
-              if (draw_data.length > 0) {
-                draw_data.forEach(async (elem, i) => {
-                  sendme = api.sendme(token, draw_data[i], "feed");
-                  sendFriends = api.sendFriends(token,draw_data[i],idList,"feed");
-                });
-              } else {
-                if (today_data.length == 1) {
-                  sendme = api.sendme(token, today_data[0], "feed");
-                  sendFriends = api.sendFriends(token,today_data[0],idList,"feed");
-                }
-              }
-            } else {
+              draw_data.forEach(async (elem, i) => {
+                sendme = api.sendme(token, draw_data[i], "feed");
+                sendFriends = api.sendFriends(token,draw_data[i],idList,"feed");
+              });
+            }
+            else {
               res.send("no data");
             }
-          } catch (e) {
+          }
+          //출시까지 함께 보낼때
+          // try {
+          //   let sendme;
+          //   let sendFriends;
+          //   //send
+          //   if (today_data.length > 0) {
+          //     if (today_data.length > 1) {
+          //       sendme = api.sendme(token, today_data, "list");
+          //       sendFriends = api.sendFriends(token, today_data, idList, "list");
+          //     }
+          //     if (draw_data.length > 0) {
+          //       draw_data.forEach(async (elem, i) => {
+          //         sendme = api.sendme(token, draw_data[i], "feed");
+          //         sendFriends = api.sendFriends(token,draw_data[i],idList,"feed");
+          //       });
+          //     } else {
+          //       if (today_data.length == 1) {
+          //         sendme = api.sendme(token, today_data[0], "feed");
+          //         sendFriends = api.sendFriends(token,today_data[0],idList,"feed");
+          //       }
+          //     }
+          //   } else {
+          //     res.send("no data");
+          //   }
+          // } 
+          catch (e) {
             console.log(e);
             res.json(e);
           }
